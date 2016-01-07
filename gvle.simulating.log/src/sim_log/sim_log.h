95388c04369a1208688b5bcb3f40dacc5431b976
Ronan Trépos <rtrepos@toulouse.inra.fr>
2016-01-07 09:11:26 +0100

    gvle.simulating.log: upgrade to vle master
    - update cmajke scripts
    - rename gvle2 with gvle
    - package added to the distribution

---
+++ gvle.simulating.log/src/sim_log/sim_log.h
@@ -0,0 +1,78 @@
+/*
+ * This file is part of VLE, a framework for multi-modeling, simulation
+ * and analysis of complex dynamical systems.
+ * http://www.vle-project.org
+ *
+ * Copyright (c) 2014 INRA
+ *
+ */
+#ifndef GVLE_SIM_LOG_H
+#define GVLE_SIM_LOG_H
+
+#include <QObject>
+#include <QList>
+#include <QSettings>
+#include <QTreeWidgetItem>
+#include <QWidget>
+#include <QThread>
+#include <vle/gvle/plugin_sim.h>
+#include <vle/gvle/logger.h>
+#include "thread.h"
+#include "simtab.h"
+#include "widtoolbar.h"
+
+namespace vle {
+namespace gvle {
+
+
+class SimLog : public QObject, public PluginSimulator
+{
+    Q_OBJECT
+    Q_INTERFACES(vle::gvle::PluginSimulator)
+
+public:
+    SimLog();
+    ~SimLog();
+    QString getname();
+    QWidget *getWidget();
+    void     delWidget();
+    QWidget *getWidgetToolbar();
+    void     delWidgetToolbar();
+    void setSettings(QSettings *s);
+    void setLogger(Logger *logger);
+    void init(vleVpm *vpm);
+    void *getVpm();
+    void setPackage(vle::utils::Package *pkg);
+
+public slots:
+    void startStop();
+    void addPlotSig(QString view, QString port);
+    void delPlotSig(QString port);
+    void simulationGetStep();
+    void simulationFinished();
+
+private:
+    void log(QString message);
+    void simulationStart();
+
+private:
+    QString name;
+    QSettings             *  mSettings;
+    Logger                *  mLogger;
+    SimTabLog             *  mWidgetTab;
+    widToolbar            *  mWidgetToolbar;
+    vleVpm                *  mVpm;
+    vle::utils::Package   *  mCurrPackage;
+    QList<QString>           mObsPorts;
+    QThread               *  mThread;
+    simLogThread         *  mSimThread;
+    vle::value::Map       *  mOutputs;
+    QMap<QString, int>       mViewLastRow;
+    bool                     mSimDataValid;
+    int                      mStepCount;
+    double                   mDuration;
+};
+
+}} //namespaces
+
+#endif // SIM_PLOT_H
---
+++ gvle.simulating.log/src/sim_log/widtoolbar.h
@@ -0,0 +1,56 @@
+/*
+ * This file is part of VLE, a framework for multi-modeling, simulation
+ * and analysis of complex dynamical systems.
+ * http://www.vle-project.org
+ *
+ * Copyright (c) 2014 INRA
+ *
+ */
+#ifndef WIDTOOLBAR_H
+#define WIDTOOLBAR_H
+
+#include <QList>
+#include <QWidget>
+#include <QTreeWidgetItem>
+#include <vle/vpz/Vpz.hpp>
+#include "vle/vpz/AtomicModel.hpp"
+
+namespace Ui {
+class widToolbar;
+}
+
+class widToolbar : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit widToolbar(QWidget *parent = 0);
+    ~widToolbar();
+    void buildTree(vle::vpz::Vpz *vpz);
+    int  sigCount();
+    void simStarted();
+    void simPaused();
+    void simStoped();
+
+public slots:
+    void onTreeItemSelected();
+    void onTreeItemChanged(QTreeWidgetItem * item, int column);
+
+private slots:
+    void onToolColor();
+
+signals:
+
+    void addSig(QString, QString);
+    void delSig(QString);
+
+private:
+    void insertViewPort(QString viewName, QString portName, QString portInfo);
+    void updateViews();
+
+private:
+    Ui::widToolbar *ui;
+    QList<QTreeWidgetItem *> mViewsItems;
+};
+
+#endif // WIDTOOLBAR_H
