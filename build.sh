92bcc2d007b8de284cba856072c6ee79807950cb
Ronan Trépos <rtrepos@toulouse.inra.fr>
2016-02-01 17:28:37 +0100

    remove vle.forrester:
    - vle.forrester relied on gtkmm

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ vle.discrete-time.generic vle.extension.cellqss vle.extension.decision
 vle.discrete-time.decision vle.reader vle.tester vle.recursive 
 vle.extension.difference-equation vle.extension.differential-equation
 vle.extension.differential-equation_test vle.extension.dsdevs vle.extension.fsa
-vle.extension.petrinet vle.examples ext.muparser vle.forrester
+vle.extension.petrinet vle.examples ext.muparser
 vle.reader_test vle.recursive_test vle.discrete-time_test
 vle.tester_test"
 
