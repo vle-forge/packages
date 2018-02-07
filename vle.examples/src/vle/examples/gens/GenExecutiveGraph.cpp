3648d3cd23681ff0b1e59c74ff01d9c32cf893cd
Patrick Chabrier <patrick.chabrier@inra.fr>
2018-02-07 14:47:03 +0100

    vle.examples: add an include

--- vle.examples/src/vle/examples/gens/GenExecutiveGraph.cpp
+++ vle.examples/src/vle/examples/gens/GenExecutiveGraph.cpp
@@ -24,7 +24,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include <functional>
 #include <vle/devs/Executive.hpp>
 #include <vle/translator/GraphTranslator.hpp>
 #include <vle/utils/Tools.hpp>
