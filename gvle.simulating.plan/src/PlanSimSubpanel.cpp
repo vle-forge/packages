2e3eb3de4e59796590ca7b831cb406f8f759933d
eric.casellas <eric.casellas@inra.fr>
2018-06-04 17:11:40 +0200

    gvle.simulating.plan: fix virtual usage

--- gvle.simulating.plan/src/PlanSimSubpanel.cpp
+++ gvle.simulating.plan/src/PlanSimSubpanel.cpp
@@ -49,22 +49,34 @@ struct sim_log : utils::Context::LogFunctor
     {
     }
 
-    virtual void write(const utils::Context& ctx,
-                       int priority,
-                       const char* file,
-                       int line,
-                       const char* fn,
-                       const char* format,
-                       va_list args) noexcept override
+    void write(const utils::Context& ctx,
+               int priority,
+               const std::string& msg) noexcept override
+    {
+        (void)ctx;
+        (void)priority;
+
+        log_messages.emplace_back(msg);
+    }
+
+    void write(const utils::Context& ctx,
+               int priority,
+               const char* file,
+               int line,
+               const char* fn,
+               const char* format,
+               va_list args) noexcept override
     {
         (void)ctx;
         (void)priority;
         (void)file;
         (void)line;
         (void)fn;
+
         log_messages.emplace_back(vle::utils::vformat(format, args));
     }
-    virtual ~sim_log(){};
+
+    ~sim_log() override = default;
 };
 
 PlanSimSubpanelThread::PlanSimSubpanelThread(
