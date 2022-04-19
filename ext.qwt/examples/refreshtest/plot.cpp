4f07450a41ffa6c4a69803a18755059d2cd726b2
Gauthier Quesnel <gauthier.quesnel@inrae.fr>
2022-04-19 14:16:01 +0200

    ext.qwt: bump to qwt-6.1.6

--- ext.qwt/examples/refreshtest/plot.cpp
+++ ext.qwt/examples/refreshtest/plot.cpp
@@ -52,8 +52,12 @@ protected:
         if ( plot )
             plot->drawCanvas( &painter );
 
-        painter.setPen( palette().foreground().color() );
+        painter.setPen( palette().windowText().color() );
+#if QT_VERSION >= 0x050000
+        painter.drawRect( rect().adjusted( 1, 1, 0, 0 ) );
+#else
         painter.drawRect( rect().adjusted( 0, 0, -1, -1 ) );
+#endif
     }
 };
 #endif
